05cf16da81c3d065c82ccf499e51b52ccecbbc3c
Waad-Raggam <108654364+Waad-Raggam@users.noreply.github.com>
2024-05-22 02:29:28 +0300

    Setting up project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
