4c2a1908106a713b46e0313eb18dd1835902283c
Waad-Raggam <108654364+Waad-Raggam@users.noreply.github.com>
2024-05-22 02:47:12 +0300

    Adding the cards to landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,7 +3,37 @@ const Landing = () => {
     return (
         <>
         <MainLayout>
-            Landing
+        <div className="container">
+                <div className="row text-center d-flex justify-content-center-mt-5">
+                    <div className="col-sm-4">
+                        <div className="card" class="card1">
+                            <div className="card-body">
+                                <i className="bi bi-phone text-center">
+                                    <h6 className="align-middle">smart phones</h6>
+                                </i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <i className="bi bi-phone text-center">
+                                    <h6 className="align-middle">smart phones</h6>
+                                </i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <i className="bi bi-phone text-center">
+                                    <h6 className="align-middle">smart phones</h6>
+                                </i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </MainLayout>
         </>
     );
