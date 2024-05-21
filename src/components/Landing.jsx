79fd03118a5ebfbda1a16be1eb7d180e69f074a5
Waad-Raggam <108654364+Waad-Raggam@users.noreply.github.com>
2024-05-22 02:43:27 +0300

    Setting up main layout and landing page
    Setting up main layout and landing page and navbar from mdb website

---
+++ src/components/Landing.jsx
@@ -0,0 +1,12 @@
+import MainLayout from "../layouts/MainLayout";
+const Landing = () => {
+    return (
+        <>
+        <MainLayout>
+            Landing
+        </MainLayout>
+        </>
+    );
+}
+
+export default Landing;
