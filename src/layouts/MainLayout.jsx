a41f1272d4a66070587706593caf62fc5e0f1533
Waad-Raggam <108654364+Waad-Raggam@users.noreply.github.com>
2024-05-23 22:13:46 +0300

    Improve navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -20,101 +20,24 @@ const MainLayouts = ({ children }) => {
                         <i class="fas fa-bars"></i>
                     </button>
 
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                        <a class="navbar-brand mt-2 mt-lg-0" href="#">
-                            <img
-                                src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-                                height="15"
-                                alt="MDB Logo"
-                                loading="lazy"
-                            />
-                        </a>
-
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Dashboard</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Team</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Projects</a>
-                            </li>
-                        </ul>
-
+                    <div class="input-group rounded">
+                        <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                        <span class="input-group-text border-0" id="search-addon">
+                            <i class="fas fa-search"></i>
+                        </span>
                     </div>
 
-
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">تسجيل حساب جديد</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">تسجيل الدخول</a>
+                        </li>
+                    </ul>
 
                     <div class="d-flex align-items-center">
-
-                        <a class="text-reset me-3" href="#">
-                            <i class="fas fa-shopping-cart"></i>
-                        </a>
-
-
-                        <div class="dropdown">
-                            <a
-                                data-mdb-dropdown-init
-                                class="text-reset me-3 dropdown-toggle hidden-arrow"
-                                href="#"
-                                id="navbarDropdownMenuLink"
-                                role="button"
-                                aria-expanded="false"
-                            >
-                                <i class="fas fa-bell"></i>
-                                <span class="badge rounded-pill badge-notification bg-danger">1</span>
-                            </a>
-                            <ul
-                                class="dropdown-menu dropdown-menu-end"
-                                aria-labelledby="navbarDropdownMenuLink"
-                            >
-                                <li>
-                                    <a class="dropdown-item" href="#">Some news</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#">Another news</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="dropdown">
-                            <a
-                                data-mdb-dropdown-init
-                                class="dropdown-toggle d-flex align-items-center hidden-arrow"
-                                href="#"
-                                id="navbarDropdownMenuAvatar"
-                                role="button"
-                                aria-expanded="false"
-                            >
-                                <img
-                                    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                                    class="rounded-circle"
-                                    height="25"
-                                    alt="Black and White Portrait of a Man"
-                                    loading="lazy"
-                                />
-                            </a>
-                            <ul
-                                class="dropdown-menu dropdown-menu-end"
-                                aria-labelledby="navbarDropdownMenuAvatar"
-                            >
-                                <li>
-                                    <a class="dropdown-item" href="#">My profile</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#">Settings</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#">Logout</a>
-                                </li>
-                            </ul>
-                        </div>
+                        <i class="bi bi-cart3"></i>
                     </div>
 
                 </div>
@@ -130,7 +53,7 @@ const MainLayouts = ({ children }) => {
                     right: '0',
                     bottom: '0'
                 }}>
-                here is my footer
+                    here is my footer
                 </div>
             </footer>
         </>
